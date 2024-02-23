02c7c2c2f348629241504d88adc808dcf13a6098
krystiano13 <crystianotv@gmail.com>
2024-02-23 18:06:00 +0100

    create text part of home view

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
-
 function App() {
-  return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div
+            class="bg-home-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] flex
+            justify-around items-end p-24 pb-36">
+            <section class="max-w-[50%] flex flex-col gap-1">
+                <h3 class="font1 text-2xl text-[#D0D6F9]">SO, YOU WANT TO TRAVEL TO</h3>
+                <h1 class="font2 text-[9rem] text-white">SPACE</h1>
+                <p class="font1 max-w-[27rem] text-xl text-[#D0D6F9]">
+                    Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not
+                    hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this
+                    world experience!
+                </p>
+            </section>
+            <button>
+              EXPLORE
+            </button>
+        </div>
+    );
 }
 
 export default App;
