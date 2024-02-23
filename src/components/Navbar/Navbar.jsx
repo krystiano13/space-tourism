c3506a83653f459ccb80b2cf80f3a984071150ac
krystiano13 <crystianotv@gmail.com>
2024-02-23 20:26:28 +0100

    create routes folder for navbar component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,23 @@
 import Logo from '../../assets/shared/logo.svg';
+import { createStore } from "solid-js/store";
+import {routes} from "./routes";
+import {For} from "solid-js";
+
 export function Navbar() {
+    const [navRoutes, setNavRoutes] = createStore(routes);
+
     return (
-        <nav class="fixed w-full p-12">
-            <img src={Logo} alt="logo" />
-            <div id="line"></div>
-            <div>
-                <a href="/">HOME</a>
-                <a href="/destination">DESTINATION</a>
-                <a href="/crew">CREW</a>
-                <a href="/technology">TECHNOLOGY</a>
+        <nav class="fixed w-full flex items-center">
+            <section class="flex w-[17%] items-center justify-center w">
+                <img class="p-12" src={Logo} alt="logo" />
+            </section>
+            <div id="line" class="w-[33%] h-0.5 opacity-70 bg-gray-600"></div>
+            <div class="w-1/2 flex justify-center items-center gap-12">
+               <For each={navRoutes}>
+                   {(route) => (
+                       <a href={route.href}>{route.name}</a>
+                   )}
+               </For>
             </div>
         </nav>
     )
---
+++ src/components/Navbar/routes.js
@@ -0,0 +1,6 @@
+export const routes = [
+    { name : "HOME", href : "/" },
+    { name : "DESTINATION", href : "/destination" },
+    { name : "CREW", href : "/crew" },
+    { name : "TECHNOLOGY", href : "/technology" },
+];
