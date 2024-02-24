5c16e101074d8ad90e4007e973e8969ae87acdc8
krystiano13 <crystianotv@gmail.com>
2024-02-24 13:00:48 +0100

    create left section of destination view

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -1,11 +1,17 @@
 import './Destination.css';
+import Moon from '../../assets/destination/image-moon.webp'
 export default function Destination() {
     return (
         <div
-            class="bg-home-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] flex
-            justify-around items-end p-28 overflow-hidden"
+            class="flex flex-col justify-center bg-dest-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] p-28 overflow-hidden"
         >
-
+            <h1 class="font1 mt-24 font-light text-4xl text-white">PICK YOUR DESTINATION</h1>
+            <div class="flex justify-around items-center w-full">
+                <img class="mt-12" src={Moon} alt="moon"/>
+                <section class="w-[50%]">
+                    123
+                </section>
+            </div>
         </div>
     )
 }
