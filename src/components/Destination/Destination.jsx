d83ab101db6c8ed838f2072388a3fb46c9e08c33
krystiano13 <crystianotv@gmail.com>
2024-02-24 13:26:11 +0100

    finish html content of the destination page

--- src/components/Destination/Destination.jsx
+++ src/components/Destination/Destination.jsx
@@ -9,8 +9,13 @@ export default function Destination() {
             <h1 class="font1 mt-24 font-light text-4xl text-white">PICK YOUR DESTINATION</h1>
             <div class="flex justify-between items-center w-full pl-12 pr-12 xl:pl-24 xl:pr-24">
                 <img class="mt-12" src={Moon} alt="moon"/>
-                <section class="w-[40%] flex flex-col gap-10">
-                    <div id="planets"></div>
+                <section class="w-[40%] flex flex-col gap-12">
+                    <div id="planets" class="flex gap-7">
+                        <button class="font1 text-[#D0D6F9] text-lg">MOON</button>
+                        <button class="font1 text-[#D0D6F9] text-lg">MARS</button>
+                        <button class="font1 text-[#D0D6F9] text-lg">EUROPA</button>
+                        <button class="font1 text-[#D0D6F9] text-lg">TITAN</button>
+                    </div>
                     <h2 class="font2 text-white text-8xl">MOON</h2>
                     <p class="font1 font-light text-justify text-[#D0D6F9] 2xl:max-w-[90%] text-xl">
                         See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain
@@ -18,6 +23,16 @@ export default function Destination() {
                         Luna 2 and Apollo 11 landing sites.
                     </p>
                     <div id="line" class="h-0.5 opacity-70 bg-gray-600 2xl:max-w-[90%] w-full"></div>
+                    <section class="2xl:max-w-[90%] w-full flex justify-start gap-16">
+                        <div class="flex flex-col gap-2">
+                            <h4 class="text-[#D0D6F9] font-light font1 text-lg">AVG. DISTANCE</h4>
+                            <p class="text-white font2 text-3xl">384,000 KM</p>
+                        </div>
+                        <div class="flex flex-col gap-2">
+                            <h4 class="text-[#D0D6F9] font-light font1 text-lg">EST. TRAVEL TIME</h4>
+                            <p class="text-white font2 text-3xl">3 DAYS</p>
+                        </div>
+                    </section>
                 </section>
             </div>
         </div>
