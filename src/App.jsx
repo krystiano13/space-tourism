1cd877b4078e59260790b156605c302896efb920
krystiano13 <crystianotv@gmail.com>
2024-02-24 12:47:40 +0100

    create routing for destination subpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Router } from "@solidjs/router";
 import Home from "./components/Home/Home";
+import Destination from "./components/Destination/Destination";
 import {Navbar} from "./components/Navbar/Navbar";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
             <Navbar />
             <Router>
                 <Route path="/" component={Home} />
+                <Route path="/destination" component={Destination} />
             </Router>
         </>
     )
---
+++ src/components/Destination/Destination.jsx
@@ -0,0 +1,11 @@
+import './Destination.css';
+export default function Destination() {
+    return (
+        <div
+            class="bg-home-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] flex
+            justify-around items-end p-28 overflow-hidden"
+        >
+
+        </div>
+    )
+}
