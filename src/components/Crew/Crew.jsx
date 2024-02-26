07400e356a35c075e2e5a1f9cd393c7b70668bfc
krystiano13 <crystianotv@gmail.com>
2024-02-26 13:09:13 +0100

    create image section of crew view

--- src/components/Crew/Crew.jsx
+++ src/components/Crew/Crew.jsx
@@ -1,13 +1,14 @@
 import './Crew.css';
+import douglas from '../../assets/crew/image-douglas-hurley.webp';
 
 export default function Crew() {
     return (
         <div
-            class="flex flex-col justify-center bg-dest-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] p-24 overflow-hidden"
+            class="flex flex-col justify-center bg-dest-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] p-24 pb-0 overflow-hidden"
         >
             <h1 class="font1 mt-24 font-light text-4xl text-white">MEET YOUR CREW</h1>
             <div class="flex justify-between items-center w-full">
-                <section id="description" className="w-[40%] flex flex-col gap-8 mt-12">
+                <section id="description" className="w-[40%] flex flex-col gap-8 mt-12 pb-24">
                     <h2 className="font2 entry2 text-white text-4xl">COMMANDER</h2>
                     <h2 className="font2 entry2 text-white text-6xl">DOUGLAS HURLEY</h2>
                     <p className="font1 entry2 font-light text-justify text-[#D0D6F9] 2xl:max-w-[90%] text-xl 2xl:text-[1.35rem]">
@@ -16,7 +17,9 @@ export default function Crew() {
                         He launched into space for the third time as commander of Crew Dragon Demo-2.
                     </p>
                 </section>
-                <img />
+                <section class="h-full flex items-end entry">
+                    <img className="h-full" src={douglas} alt="Douglas Hurley Photo"/>
+                </section>
             </div>
         </div>
     )
