9d22df7613d89fd516e49e1892a39097061aed24
krystiano13 <crystianotv@gmail.com>
2024-02-26 12:59:43 +0100

    create routing for Crew view

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import { Route, Router } from "@solidjs/router";
+import {Navbar} from "./components/Navbar/Navbar";
+
+// Views
 import Home from "./components/Home/Home";
 import Destination from "./components/Destination/Destination";
-import {Navbar} from "./components/Navbar/Navbar";
+import Crew from './components/Crew/Crew';
 
 export default function App() {
     return (
@@ -10,6 +13,7 @@ export default function App() {
             <Router>
                 <Route path="/" component={Home} />
                 <Route path="/destination" component={Destination} />
+                <Route path="/crew" component={Crew} />
             </Router>
         </>
     )
---
+++ src/components/Crew/Crew.jsx
@@ -0,0 +1,11 @@
+import './Crew.css';
+
+export default function Crew() {
+    return (
+        <div
+            className="bg-dest-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] flex
+            justify-around items-end p-28 overflow-hidden">
+
+        </div>
+    )
+}
