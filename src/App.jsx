2f191a9b2d06c5ed2565eedc100c8cb8f08f955c
krystiano13 <crystianotv@gmail.com>
2024-02-27 12:47:50 +0100

    create route for technology view

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Router } from "@solidjs/router";
-import {Navbar} from "./components/Navbar/Navbar";
+import { Navbar } from "./components/Navbar/Navbar";
 
 // Views
 import Home from "./components/Home/Home";
 import Destination from "./components/Destination/Destination";
 import Crew from './components/Crew/Crew';
+import Technology from "./components/Technology/Technology";
 
 export default function App() {
     return (
@@ -14,6 +15,7 @@ export default function App() {
                 <Route path="/" component={Home} />
                 <Route path="/destination" component={Destination} />
                 <Route path="/crew" component={Crew} />
+                <Route path="/technology" component={Technology} />
             </Router>
         </>
     )
---
+++ src/components/Technology/Technology.jsx
@@ -0,0 +1,9 @@
+import "./Technology.css";
+
+export default function Technology() {
+    return (
+        <div
+            class="flex flex-col justify-end bg-dest-moon bg-cover bg-no-repeat w-[100vw] h-[100vh] p-24 pb-0 overflow-hidden"
+        ></div>
+    )
+}
