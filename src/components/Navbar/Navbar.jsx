6c1b20d6f509d3c7ad9a022c283bb787532a5c70
krystiano13 <crystianotv@gmail.com>
2024-02-29 11:12:15 +0100

    create styles for hamburger button in navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,12 +8,12 @@ export function Navbar() {
     const [navRoutes, setNavRoutes] = createStore(routes);
 
     return (
-        <nav class="fixed w-full flex items-center">
+        <nav class="fixed w-full flex justify-between md:justify-start items-center p-12">
             <section class="flex w-[17%] items-center justify-center w">
-                <img class="p-12 scale-125" src={Logo} alt="logo" />
+                <img class="md:p-12 scale-125" src={Logo} alt="logo" />
             </section>
-            <div id="line" class="w-[33%] translate-x-6 z-50 h-0.5 opacity-70 bg-gray-600"></div>
-            <div id="navElements" class="w-1/2 bg-[#979797] h-[5rem] flex justify-center gap-12">
+            <div id="line" class="w-[33%] hidden md:block translate-x-6 z-50 h-0.5 opacity-70 bg-gray-600"></div>
+            <div id="navElements" class="w-1/2 bg-[#979797] h-[5rem] hidden md:flex justify-center gap-12">
                <For each={navRoutes}>
                    {(route) => (
                        <a
@@ -24,6 +24,13 @@ export function Navbar() {
                        </a>
                    )}
                </For>
+            </div>
+            <div id="hamburgerButton"
+                 class="cursor-pointer flex flex-col justify-around items-center w-12 h-12 md:hidden"
+            >
+                <div id="btnLine" class="w-full h-1 bg-white"></div>
+                <div id="btnLine" class="w-full h-1 bg-white"></div>
+                <div id="btnLine" class="w-full h-1 bg-white"></div>
             </div>
         </nav>
     )
