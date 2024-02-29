37f42e32b25cd186c4c0cf8e7e5d4fd85680d5ac
krystiano13 <crystianotv@gmail.com>
2024-02-29 10:34:21 +0100

    add data about mobile images of technology view

--- src/components/Technology/info.js
+++ src/components/Technology/info.js
@@ -1,9 +1,16 @@
+// Portrait
 import launch from '../../assets/technology/image-launch-vehicle-portrait.jpg';
 import spaceport from '../../assets/technology/image-spaceport-portrait.jpg';
 import capsule from '../../assets/technology/image-space-capsule-portrait.jpg';
+
+// Landscape
+import launchMobile from '../../assets/technology/image-launch-vehicle-landscape.jpg';
+import spaceportMobile from '../../assets/technology/image-spaceport-landscape.jpg';
+import capsuleMobile from '../../assets/technology/image-space-capsule-landscape.jpg';
+
 /**
  *
- * @type {[{img: {}, alt: string, id: number, title: string, desc: string},{img: {}, alt: string, id: number, title: string, desc: string},{img: {}, alt: string, id: number, title: string, desc: string}]}
+ * @type {[{img: {}, alt: string, id: number, title: string, img2: {}, desc: string},{img: {}, alt: string, id: number, title: string, img2: {}, desc: string},{img: {}, alt: string, id: number, title: string, img2: {}, desc: string}]}
  */
 export const info = [
     {
@@ -11,6 +18,7 @@ export const info = [
         title: "LAUNCH VEHICLE",
         desc: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
         img : launch,
+        img2: launchMobile,
         alt: "space rocket"
     },
     {
@@ -18,6 +26,7 @@ export const info = [
         title: "SPACEPORT",
         desc: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
         img : spaceport,
+        img2 : spaceportMobile,
         alt: "spaceport"
     },
     {
@@ -25,6 +34,7 @@ export const info = [
         title: "SPACE CAPSULE",
         desc: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
         img : capsule,
+        img2 : capsuleMobile,
         alt: "space capsule"
     },
 ];
