7be8945bcb28bd0421f23ae799b910c53ede78b9
krystiano13 <crystianotv@gmail.com>
2024-02-29 10:58:24 +0100

    create styles for large tablets in technology view

--- src/components/Technology/Technology.jsx
+++ src/components/Technology/Technology.jsx
@@ -13,15 +13,15 @@ export default function Technology() {
             </h1>
             <div class="flex flex-col-reverse lg:flex-row justify-between items-center w-full h-4/5">
                 <section class="w-full pt-4 pb-4 lg:pt-0 lg:pb-0 lg:w-[60%] flex flex-col lg:flex-row gap-12 lg:gap-24 h-3/5 entry">
-                    <div class="flex flex-row lg:flex-col justify-evenly lg:justify-between items-center gap-6 h-full">
+                    <div class="flex flex-row lg:flex-col justify-evenly xl:justify-between items-center gap-6 h-full">
                         <button onClick={() => setId(0)} class={`${id() === 0 && "bg-white text-black"} text-white transition rounded-full w-16 md:w-24 lg:w-16 2xl:w-24 h-16 md:h-24 lg:h-16 2xl:h-24 text-3xl font2 border-[2px] border-gray-400 hover:border-white`}>1</button>
                         <button onClick={() => setId(1)} class={`${id() === 1 && "bg-white text-black"} text-white transition rounded-full w-16 md:w-24 lg:w-16 2xl:w-24 h-16 md:h-24 lg:h-16 2xl:h-24 text-3xl font2 border-[2px] border-gray-400 hover:border-white`}>2</button>
                         <button onClick={() => setId(2)} class={`${id() === 2 && "bg-white text-black"} text-white transition rounded-full w-16 md:w-24 lg:w-16 2xl:w-24 h-16 md:h-24 lg:h-16 2xl:h-24 text-3xl font2 border-[2px] border-gray-400 hover:border-white`}>3</button>
                     </div>
-                    <section class="flex flex-col items-center lg:items-start justify-between gap-5 lg:gap-6 h-full">
+                    <section class="flex flex-col items-center lg:items-start lg:justify-center xl:justify-between gap-5 lg:gap-6 h-full">
                         <h3 class="text-[#D0D6F9] text-center lg:text-left font1 lg:text-xl 2xl:text-2xl">THE TERMINOLOGY</h3>
                         <h2 class="text-white font2 text-center lg:text-left md:text-4xl lg:text-5xl 2xl:text-7xl">{ info[id()].title }</h2>
-                        <p id="text" class="text-[#D0D6F9] font1 text-center max-w-[90%] text-sm md:text-lg lg:text-left lg:text-lg 2xl:text-xl lg:max-w-[75%] 2xl:max-w-[60%]">
+                        <p id="text" class="text-[#D0D6F9] font1 text-center max-w-[90%] text-sm md:text-base lg:text-left lg:text-base 2xl:text-xl lg:max-w-[75%] 2xl:max-w-[60%]">
                             { info[id()].desc }
                         </p>
                     </section>
